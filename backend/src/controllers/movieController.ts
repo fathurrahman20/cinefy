fd69bd1843ad324f1cc2b2f73ba71b1b81abac11
Fathurrahman <10190031@bsi.ac.id>
2025-06-25 14:10:41 +0700

    feat(backend): api put update movie with multer

--- backend/src/controllers/movieController.ts
+++ backend/src/controllers/movieController.ts
@@ -3,6 +3,8 @@ import Movie from "../models/Movie";
 import { movieSchema } from "../utils/schema";
 import Genre from "../models/Genre";
 import Theater from "../models/Theater";
+import path from "path";
+import fs from "node:fs";
 
 export const getMovies: RequestHandler = async (
   req: Request,
@@ -111,6 +113,129 @@ export const createMovie: RequestHandler = async (
       status: "error",
       message: "Failed to create movie",
       data: null,
+    });
+  }
+};
+
+export const updateMovie: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const { id } = req.params;
+
+    const parse = movieSchema.safeParse({
+      title: req.body.title,
+      genre: req.body.genre,
+      theaters: req.body.theaters.split(","),
+      description: req.body.description,
+      price: Number(req.body.price),
+      available: req.body.available === "1" ? true : false,
+      bonus: req.body?.bonus,
+    });
+
+    if (!parse.success) {
+      const errorMessage = parse.error.issues.map((issue) => issue.message);
+
+      res.status(400).json({
+        status: "error",
+        message: "Invalid request",
+        error: errorMessage,
+      });
+      return;
+    }
+
+    const oldMovie = await Movie.findById(id);
+
+    if (!oldMovie) {
+      res.status(404).json({
+        status: "error",
+        message: "Movie not found",
+        data: null,
+      });
+      return;
+    }
+
+    const isGenreExists = await Genre.findById(req.body.genre);
+    if (!isGenreExists) {
+      res.status(404).json({
+        status: "error",
+        message: "Genre not found",
+        data: null,
+      });
+      return;
+    }
+
+    const isTheaterExists = await Theater.findById({
+      _id: req.body.theaters.split(","),
+    });
+    if (!isTheaterExists) {
+      res.status(404).json({
+        status: "error",
+        message: "Theater not found",
+        data: null,
+      });
+      return;
+    }
+
+    if (!req.file) {
+      const dirname = path.resolve();
+      const filepath = path.join(
+        dirname,
+        "public/uploads/thumbnails",
+        oldMovie.thumbnail!
+      );
+
+      if (fs.existsSync(filepath)) {
+        fs.unlinkSync(filepath);
+      }
+    }
+
+    await Genre.findByIdAndUpdate(oldMovie.genre, {
+      $pull: { movies: oldMovie._id },
+    });
+
+    for (const theater of oldMovie.theaters) {
+      await Theater.findByIdAndUpdate(theater._id, {
+        $pull: { movies: oldMovie._id },
+      });
+    }
+
+    const updatedMovie = await Movie.findByIdAndUpdate(
+      oldMovie._id,
+      {
+        title: parse.data.title,
+        genre: parse.data.genre,
+        available: parse.data.available,
+        theaters: parse.data.theaters,
+        thumbnail: req?.file ? req.file.filename : oldMovie.thumbnail,
+        description: parse.data.description,
+        price: parse.data.price,
+        bonus: parse.data.bonus,
+      },
+      { new: true, runValidators: true }
+    );
+
+    await Genre.findByIdAndUpdate(parse.data.genre, {
+      $push: { movies: id },
+    });
+
+    for (const theater of parse.data.theaters) {
+      await Theater.findByIdAndUpdate(theater, {
+        $push: { movies: id },
+      });
+    }
+
+    res.status(200).json({
+      status: "success",
+      message: "Successfully updated movie",
+      data: updatedMovie,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Failed to update data",
+      data: null,
+      status: "failed",
     });
   }
 };
--- backend/src/routes/adminRoutes/MovieRoutes.ts
+++ backend/src/routes/adminRoutes/MovieRoutes.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
-import { createMovie, getMovies } from "../../controllers/movieController";
+import {
+  createMovie,
+  getMovies,
+  updateMovie,
+} from "../../controllers/movieController";
 import multer from "multer";
 import { imageFilter, thumbnailStorage } from "../../utils/multer";
 
@@ -9,5 +13,6 @@ export const movieRoutes = Router();
 
 movieRoutes.get("/movies", getMovies);
 movieRoutes.post("/movies", upload.single("thumbnail"), createMovie);
+movieRoutes.put("/movies/:id", upload.single("thumbnail"), updateMovie);
 
 export default movieRoutes;
