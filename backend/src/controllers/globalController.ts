f970a0510ca6665afea160038648bd9ef2c4b912
Fathurrahman <10190031@bsi.ac.id>
2025-06-27 10:34:42 +0700

    feat(backend): api for movie detail

--- backend/src/controllers/globalController.ts
+++ backend/src/controllers/globalController.ts
@@ -45,3 +45,79 @@ export const getGenre: RequestHandler = async (req: Request, res: Response) => {
     });
   }
 };
+
+export const getMovieDetail: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
+  try {
+    const seats = [];
+
+    // create 5 seats for each row
+    for (let i = 0; i < 5; i++) {
+      seats.push({
+        seat: `A${i + 1}`,
+        isBooked: false,
+      });
+    }
+
+    for (let i = 0; i < 5; i++) {
+      seats.push({
+        seat: `B${i + 1}`,
+        isBooked: false,
+      });
+    }
+
+    for (let i = 0; i < 5; i++) {
+      seats.push({
+        seat: `C${i + 1}`,
+        isBooked: false,
+      });
+    }
+
+    const movie = await Movie.findById(req.params.id)
+      .populate({
+        path: "genre",
+        select: "name -_id",
+      })
+      .populate({
+        path: "theaters",
+        select: "name city",
+      });
+
+    if (!movie) {
+      res.status(404).json({
+        status: "error",
+        message: "Movie not found",
+        data: null,
+      });
+      return;
+    }
+
+    res.status(200).json({
+      status: "success",
+      message: "Successfully fetched movie detail",
+      data: {
+        movie: {
+          ...movie?.toJSON(),
+        },
+        seats,
+        times: ["10:00", "12:00", "14:00", "16:00", "19:00"],
+      },
+    });
+  } catch (error: any) {
+    if (error.name === "CastError") {
+      res.status(400).json({
+        status: "error",
+        message: "Invalid Movie ID",
+        data: null,
+      });
+      return;
+    }
+    res.status(500).json({
+      status: "error",
+      message: "Failed to fetch movie detail",
+      data: null,
+    });
+  }
+};
--- backend/src/routes/adminRoutes/customerRoutes/globalRoutes.ts
+++ backend/src/routes/adminRoutes/customerRoutes/globalRoutes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { getGenre, getMovies } from "../../../controllers/globalController";
+import {
+  getGenre,
+  getMovieDetail,
+  getMovies,
+} from "../../../controllers/globalController";
 
 const globalRoutes = Router();
 
 globalRoutes.get("/movies", getMovies);
 globalRoutes.get("/genres", getGenre);
+globalRoutes.get("/movies/:id", getMovieDetail);
 
 export default globalRoutes;
