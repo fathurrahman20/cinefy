8982e932fc0341b98e6a76c3ee004157c1f3c055
Fathurrahman <10190031@bsi.ac.id>
2025-07-03 11:21:20 +0700

    feat: api get topup history

--- backend/src/controllers/walletController.ts
+++ backend/src/controllers/walletController.ts
@@ -1,6 +1,7 @@
 import { RequestHandler, Response } from "express";
 import { CustomRequest } from "../types/Request";
 import Wallet from "../models/Wallet";
+import WalletTransaction from "../models/WalletTransaction";
 
 export const getBalance: RequestHandler = async (
   req: CustomRequest,
@@ -20,6 +21,33 @@ export const getBalance: RequestHandler = async (
     res.status(500).json({
       status: "error",
       message: "Failed to fetch balance",
+      data: null,
+    });
+  }
+};
+
+export const getTopupHistory: RequestHandler = async (
+  req: CustomRequest,
+  res: Response
+) => {
+  try {
+    const wallet = await Wallet.findOne({
+      user: req.user?.id,
+    });
+
+    const data = await WalletTransaction.find({
+      wallet: wallet?._id,
+    }).select("wallet price createdAt status");
+
+    res.status(200).json({
+      status: "success",
+      message: "Successfully fetched data",
+      data: data,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "failed",
+      message: "Failed to get data",
       data: null,
     });
   }
--- backend/src/routes/customerRoutes/walletRoutes.ts
+++ backend/src/routes/customerRoutes/walletRoutes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { getBalance } from "../../controllers/walletController";
+import {
+  getBalance,
+  getTopupHistory,
+} from "../../controllers/walletController";
 
 const walletRoutes = Router();
 
 walletRoutes.get("/check-balance", getBalance);
+walletRoutes.get("/topup-history", getTopupHistory);
 
 export default walletRoutes;
