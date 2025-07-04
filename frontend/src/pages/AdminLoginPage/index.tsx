8643a94b77c506b4468776d27016d417b1c7462e
Fathurrahman <10190031@bsi.ac.id>
2025-07-04 21:33:51 +0700

    feat: admin login page ui

--- frontend/src/pages/AdminLoginPage/index.tsx
+++ frontend/src/pages/AdminLoginPage/index.tsx
@@ -1,3 +1,56 @@
-export default function AdminLoginPage() {
-  return <div>AdminLoginPage</div>;
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export default function AdminLoginPage({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<"div">) {
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm"></div>
+      <div className={cn("flex flex-col gap-6", className)} {...props}>
+        <Card className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+          <CardHeader>
+            <CardTitle className="text-2xl">Login</CardTitle>
+            <CardDescription>
+              Enter your email below to login to your account
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form>
+              <div className="flex flex-col gap-6">
+                <div className="grid gap-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    type="email"
+                    placeholder="m@example.com"
+                    required
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <div className="flex items-center">
+                    <Label htmlFor="password">Password</Label>
+                  </div>
+                  <Input id="password" type="password" required />
+                </div>
+                <Button type="submit" className="w-full">
+                  Login
+                </Button>
+              </div>
+            </form>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
 }
