325ae476844b01232cb89c5cc3159aec64135b59
Fathurrahman <10190031@bsi.ac.id>
2025-07-09 08:25:31 +0700

    fix(frontend): showing toast if error create genre

--- frontend/src/hooks/genre/useCreateGenre.tsx
+++ frontend/src/hooks/genre/useCreateGenre.tsx
@@ -1,6 +1,8 @@
 import type { GenreValues } from "@/lib/validation/genre";
 import { createGenre } from "@/services/auth/genre/genre.service";
+import type { ErrorResponse } from "@/types/response";
 import { useMutation } from "@tanstack/react-query";
+import type { AxiosError } from "axios";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router";
 
@@ -11,6 +13,14 @@ export const useCreateGenre = () => {
     onSuccess: () => {
       toast.success("Genre created successfully");
       navigate("/admin/genres");
+    },
+    onError: (error: AxiosError<ErrorResponse>) => {
+      if (error.response?.status === 400) {
+        return toast.error(`${error.response?.data.message}`);
+      }
+      return toast.error(
+        error.response?.data.message || "Something went wrong."
+      );
     },
   });
 };
